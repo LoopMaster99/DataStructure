e75ebdd4e94fc8946a03cc3d454098676d86bf99
LoopMaster99 <bansalkrishna909@gmail.com>
2025-03-04 07:06:32 +0530

    create tree node

---
+++ BinaryTree/TreeNode.java
@@ -0,0 +1,11 @@
+package BinaryTree;
+
+public class TreeNode {
+    int data;
+    TreeNode left, right;
+
+    TreeNode(int data){
+        this.data = data;
+        this.left = this.right = null;
+    }
+}
