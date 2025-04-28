01de97c951df4e58f08d57dbcc1a0314dbaa5c81
LoopMaster99 <bansalkrishna909@gmail.com>
2025-04-29 03:15:32 +0530

    add main method

--- StackQueue/StackUsingArray.java
+++ StackQueue/StackUsingArray.java
@@ -1,11 +1,50 @@
 package StackQueue;
 
-import java.util.Stack;
+public class StackUsingArray {
 
-public class UsingArray {
+    int top;
+    int length;
+    int[] arr; 
+
+    StackUsingArray(){
+        this.length = 10;
+        this.top = -1;
+        this.arr = new int [length];
+    }
+
+    StackUsingArray(int length){
+        this.length = length;
+        this.top = -1;
+        this.arr = new int [length];
+    }
+
+    void push(int x){
+        top = top + 1;
+        arr[top] = x;
+    }
+
+    int pop(){
+        if(top == -1) System.out.println("\"UnderFlow\", please push element first");
+        int val = arr[top];
+        top = top - 1;
+        return val;
+    }
+
+    void peek(){
+        if(top == -1) System.out.println("\"UnderFlow\", stack is empty");    
+        System.out.println(arr[top]);
+    }
+
+    void size(){
+        System.out.println(top + 1); 
+    }
+}
+
+class Main{
     public static void main(String[] args) {
-        Stack<Integer> st = new Stack<>();
-        
-        st.s
+        StackUsingArray st = new StackUsingArray();
+        st.push(9);
+        st.peek();
+        st.size();
     }
 }
