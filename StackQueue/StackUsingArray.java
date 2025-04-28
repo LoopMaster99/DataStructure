e88fbd3db57de44bbdf416ec9858d2582a46bfdd
LoopMaster99 <bansalkrishna909@gmail.com>
2025-04-29 03:14:38 +0530

    implement stack using array

---
+++ StackQueue/StackUsingArray.java
@@ -0,0 +1,11 @@
+package StackQueue;
+
+import java.util.Stack;
+
+public class UsingArray {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        
+        st.s
+    }
+}
