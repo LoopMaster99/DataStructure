e962ff5ae8837d819783b73783d4e854f9992512
LoopMaster99 <bansalkrishna909@gmail.com>
2025-08-12 07:13:33 +0530

    recursive approach for lcs

---
+++ Dynamic_Programming/LongestCommonSubsequence/Recursive.java
@@ -0,0 +1,24 @@
+package Dynamic_Programming.LongestCommonSubsequence;
+
+public class Recursive {
+    public int lcs(String s1, String s2){
+        int n = s1.length();
+        int m = s2.length();
+
+        return recursiveApproach(s1, s2, n, m);
+    }
+
+    private int recursiveApproach(String s1, String s2, int n, int m) {
+        if(n == 0 || m == 0)
+            return 0;
+        
+        if(s1.charAt(n - 1) == s2.charAt(m - 1))    
+            return 1 + recursiveApproach(s1, s2, n - 1, m - 1);
+        else{
+            return Math.max(
+                recursiveApproach(s1, s2, n - 1, m), 
+                recursiveApproach(s1, s2, n, m - 1)
+            );
+        }    
+    }
+}
