4cea2cf1cee2514ab89fabc67b0d104b5282a86a
LoopMaster99 <bansalkrishna909@gmail.com>
2025-08-27 03:05:39 +0530

    solve flood fill algorithm

---
+++ Graph/FloodFill.java
@@ -0,0 +1,29 @@
+package Graph;
+
+public class FloodFill {
+    int rows, cols;
+    
+    private void dfs(int row, int col, int[][] image, int currColor, int newColor){
+        if(row < 0 || row >= rows || col < 0 || col >= cols ||
+            image[row][col] != currColor || image[row][col] == newColor){
+                return;
+        }
+
+        image[row][col] = newColor;
+        
+        int[][] adjList = {{row - 1, col}, {row, col + 1},
+                           {row + 1, col}, {row, col - 1}};
+
+        for(int[] neighbour : adjList){
+            dfs(neighbour[0], neighbour[1], image, currColor, newColor);
+        }                   
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        rows = image.length;
+        cols = image[0].length;
+
+        dfs(sr, sc, image, image[sr][sc], color);
+
+        return image;
+    }
+}
