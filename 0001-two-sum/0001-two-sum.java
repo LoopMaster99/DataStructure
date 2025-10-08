bbdf983e1f0c33ce62bfa4a3658aa70d957548c5
Krishna Bansal <119450065+LoopMaster99@users.noreply.github.com>
2025-10-08 08:32:39 +0530

    Time: 88 ms (5.04%), Space: 44.9 MB (65.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
         int n = nums.length;
-        int i1 = 0;
-        int i2 = 0;
         int count = 0;
         for(int i = 0; i < n; i++){
             int val = nums[i];
             for(int j = 0; j < n; j++){
                 if( j != i){
-                    int sum = nums[i] + nums[j];
-                    if(sum == target){
-                        i1 = i;
-                        i2 = j;
+                    if((nums[i] + nums[j]) == target){
+                        arr[0] = i;
+                        arr[1] = j;
                         count++;
-                        break;     
+                        return arr;     
                     }
                 }
             }
@@ -21,7 +19,6 @@ class Solution {
                 break;
             }
         }
-        int[] arr = new int[]{i1,i2};
         return arr;
     }
 }
