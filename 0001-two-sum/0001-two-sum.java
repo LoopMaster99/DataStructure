eb795199808e54eaebd3e804719b9d3104cc3489
Krishna Bansal <119450065+LoopMaster99@users.noreply.github.com>
2025-10-08 21:19:32 +0530

    Time: 2 ms (98.94%), Space: 45.7 MB (5.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,22 +2,16 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] arr = new int[2];
         int n = nums.length;
-        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < n; i++){
-            int val = nums[i];
-            for(int j = 0; j < n; j++){
-                if( j != i){
-                    if((nums[i] + nums[j]) == target){
-                        arr[0] = i;
-                        arr[1] = j;
-                        count++;
-                        return arr;     
-                    }
-                }
+            int pivot1 = nums[i];
+            int pivot2 = target - pivot1;
+            if(map.containsKey(pivot2)){
+                arr[0] = map.get(pivot2);
+                arr[1] = i;
+                return arr;
             }
-            if(count == 1){
-                break;
-            }
+            map.put(pivot1,i);
         }
         return arr;
     }
