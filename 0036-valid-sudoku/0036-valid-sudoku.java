ece4400267ad7434422a361beea5bce6d1fc0257
Krishna Bansal <119450065+LoopMaster99@users.noreply.github.com>
2025-10-12 21:35:11 +0530

    Time: 2 ms (61.12%), Space: 44.4 MB (65.94%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,25 +1,30 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        Set<Character>[] rows = new HashSet[9];
+        Set<Character>[] cols = new HashSet[9];
+        Set<Character>[] boxes = new HashSet[9];
+
+        for(int i = 0; i < 9; i++){
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
-                if(!isValid(board, i, j))
-                return false;
+                char ch = board[i][j];
+
+                if(ch == '.') continue;
+
+                if(!rows[i].add(ch)) return false;
+
+                if(!cols[j].add(ch)) return false;
+
+                int boxInd = (i / 3) * 3 + (j / 3);
+                if(!boxes[boxInd].add(ch)) return false;
             }
-        }    
-        return true;    
-    }
-    private boolean isValid(char[][] board, int row, int col) {
-        if(board[row][col] == '.')
-        return true;
-        
-        for(int i = 0; i < 9; i++){
-            if(col != i && board[row][i] == board[row][col])
-                return false;
-            if(row != i && board[i][col] == board[row][col])
-                return false;
-            if((3*(row/3) + i/3) != row && (3*(col/3) + i%3) != col && board[ 3*(row/3) + i/3 ][ 3*(col/3) + i%3 ] == board[row][col])
-                return false;
         }
+
         return true;
     }
 }
