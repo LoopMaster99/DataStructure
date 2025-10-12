a6ea4d5440ccbdd7d22344906dafc6cfb4ac8ec8
Krishna Bansal <119450065+LoopMaster99@users.noreply.github.com>
2025-10-12 18:43:27 +0530

    Time: 3 ms (69.65%), Space: 42.8 MB (95.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,9 +1,16 @@
 class Solution {
-    public boolean isValid(String str){
-        int i = 0, j = str.length() - 1;
+    public boolean isPalindrome(String s) {
+        String copy = s.toLowerCase();
+        int i = 0, j = s.length() - 1;
 
         while(i < j){
-            if(str.charAt(i) == str.charAt(j)){
+            char left = copy.charAt(i), right = copy.charAt(j);
+
+            if(left == ' ') i++;
+            else if(right == ' ') j--;
+            else if(!Character.isLetter(left) && !Character.isDigit(left)) i++;
+            else if(!Character.isLetter(right) && !Character.isDigit(right)) j--;
+            else if(left == right){
                 i++;
                 j--;
             }
@@ -13,20 +20,5 @@ class Solution {
         }
 
         return true;
-    }
-    public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-
-            if(ch == ' ') continue;
-            else if(Character.isLetter(ch)  || Character.isDigit(ch)){
-                sb.append(Character.toLowerCase(ch));
-            }
-            else continue;
-        }
-
-        return (isValid(sb.toString())) ? true : false;
     }
 }
