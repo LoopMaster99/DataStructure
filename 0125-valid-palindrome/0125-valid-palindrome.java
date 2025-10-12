f1f7643352e1ffc3ae842a808217020544760769
Krishna Bansal <119450065+LoopMaster99@users.noreply.github.com>
2025-10-12 18:48:03 +0530

    Time: 3 ms (69.65%), Space: 43.1 MB (74.89%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String copy = s.toLowerCase();
         int i = 0, j = s.length() - 1;
 
         while(i < j){
-            char left = copy.charAt(i), right = copy.charAt(j);
+            char left = Character.toLowerCase((s.charAt(i))),
+             right = Character.toLowerCase((s.charAt(j)));
 
-            
             if(!Character.isLetter(left)
              && !Character.isDigit(left)) i++;
 
             else if(!Character.isLetter(right)
              && !Character.isDigit(right)) j--;
-             
+
             else if(left == right){
                 i++;
                 j--;
