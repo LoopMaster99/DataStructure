11f10ef3c16363e66d1f4c1d12f9db5b73a050a1
Krishna Bansal <119450065+LoopMaster99@users.noreply.github.com>
2025-10-16 07:47:03 +0530

    Time: 5 ms (81.49%), Space: 42 MB (38.22%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -7,22 +7,20 @@ class Solution {
 
         int start = 0, end = 0;
         while(end < n){
-            while(end <= n - 2 && nums[end] + 1 == nums[end + 1]){
+            while(end < n - 1 && nums[end] + 1 == nums[end + 1]){
                 end++;
             }
 
             if(nums[start] == nums[end]){
-                String str = String.valueOf(nums[end]);
-                list.add(str);
-                start++;
+                list.add(String.valueOf(nums[end]));
             }
             else{
                 String str = nums[start] + "->" + nums[end];
                 list.add(str);
-                start = (end < n - 1) ? end + 1 : end;
             }
 
             end++;
+            start = end;
         }
 
         return list;
