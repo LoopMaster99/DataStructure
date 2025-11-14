f7132e02c2ce8bca9ed0d1dcafeb863d543a881e
Krishna Bansal <bansalkrishna909@gmail.com>
2025-11-14 18:01:11 +0530

    Time: 5 ms (77.95%), Space: 77.2 MB (7.28%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,11 +3,10 @@ class Solution {
         int n = height.length;
         int left = 0, right = n - 1;
 
-        int min = Integer.MAX_VALUE;
         int area = Integer.MIN_VALUE;
         while(left < right){
             int width = right - left;
-            min = Math.min(height[left], height[right]);
+            int min = Math.min(height[left], height[right]);
             area = Math.max(area, width * min);
 
             if(height[left] < height[right]) left++;
